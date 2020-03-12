a31f8aab7bb949e79b413fa8a215ff5cdaa5f681
golosio <golosio@unica.it>
2020-03-12 01:03:42 +0100

    set sha256 checksum for neurongpu

--- Casks/neurongpu.rb
+++ Casks/neurongpu.rb
@@ -1,6 +1,6 @@
 cask 'neurongpu' do
   version '1.0.8'
-  sha256 :no_check
+  sha256 '00892366728978b3dd3ba7f5fd38e3e304bf5459a67daeec536bd572d8c7ed87'
 
   url "https://github.com/golosio/NeuronGPU/releases/download/macOS-v1.0.8/NeuronGPU-macOS-1.0.8.tgz"
   name 'Neurongpu'
